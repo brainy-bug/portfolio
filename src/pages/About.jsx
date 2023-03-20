61ada50e2964d4228d095f2cfb81327e08035edd
maodesigns <mao.devv@gmail.com>
2023-03-20 10:52:29 +0100

    Updated text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,8 +17,8 @@ const About = () => {
         </p>
         <p>
           My job is to build your website so that it is functional and
-          user-friendly but at the same time attractive. Moreover, I add
-          personal touch to your product and make sure that is eye-catching and
+          user-friendly but at the same time, attractive. Moreover, I add
+          personal touch to your product and make sure that it is eye-catching and
           easy to use. My aim is to bring across your message and identity in
           the most creative way.
         </p>
