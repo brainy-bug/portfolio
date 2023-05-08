c67994cca9f31228225e3c4e4d00a5d3bb493655
maodesigns <mao.devv@gmail.com>
2023-05-08 09:49:07 +0100

    removed alert

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -5,16 +5,6 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
 const SharedLayout = () => {
-    useEffect(() => {
-      alert(
-        `Welcome to my portfolio website! 🥰
-
-I am currently working hard to make this site a comprehensive representation of my skills and experiences. While it's still under construction, I invite you to take a look around and see what's already available.
-
-Thank you for your patience! 🤗🤗`
-      );
-    }, []);
-
   return (
     <main>
       <Sidebar />
