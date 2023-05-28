515e309d685c72b3a09f987912ce1ee26bf0d118
maodesigns <mao.devv@gmail.com>
2023-05-29 00:18:43 +0100

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,7 @@
 import SharedLayout from "./SharedLayout";
 import About from "./About";
 import Resume from "./Resume";
-import Portfolio from "./Projects";
+import Projects from "./Projects";
 import Error from "./Error";
 import Contact from "./Contact";
-export {
-  SharedLayout,
-  About,
-  Resume,
-  Portfolio,
-  Contact,
-  Error,
-};
+export { SharedLayout, About, Resume, Projects, Contact, Error };
